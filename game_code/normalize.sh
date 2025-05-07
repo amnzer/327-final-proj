4b8ef6158ff174492398b2cee4e30985db747aca
amnzer <em.zerefa@gmail.com>
2025-05-07 17:36:41 -0500

    minor edits + renamed repo

--- game_code/normalize.sh
+++ game_code/normalize.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 # normalize all audio files recursively in /oszs
-find ../oszs/ -type f -name "*.mp3" | while read -r file; do
+find /Users/donu/Desktop/S25/ELEC\ 327/327-final-proj/oszs/ -type f -name "*.mp3" | while read -r file; do
   tmpfile="$(dirname "$file")/.__tmp_normalized.mp3"
   echo tmpfile
   ffmpeg -nostdin -i "$file" -af loudnorm -y "$tmpfile" && mv "$tmpfile" "$file"
