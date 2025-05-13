ec68164e51cec182e91b8cc8f879c686e0da4b08
amnzer <em.zerefa@gmail.com>
2025-05-13 12:37:19 -0700

    caught some tiny typos

---
+++ website/tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        pixeboy: ['pixeboy'], // use `font-myfont` in class
+      },
+    },
+  },
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx,svelte,vue}",
+  ],
+}
